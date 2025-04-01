00f72648b8197a1515a67bd172422ca4129d6ed4
Farhad <farhadrezaeibusiness@gmail.com>
2025-04-01 18:55:54 +0330

    فایل های مورد نیاز برای دیتابیس

---
+++ models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    tasks = db.relationship("Task", backref="user", lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    date = db.Column(db.String(50), nullable=True)
+    priority = db.Column(db.String(10), default="medium")
+    completed = db.Column(db.Boolean, default=False)
+    tags = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
